2f36870788d3b958d11c228d9c24c916c3f867da
Lewey Metcalf <leweymetcalf@hotmail.co.uk>
2020-01-27 09:27:55 +0100

    add percolation implementation

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,25 +1,108 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 
-    // creates n-by-n grid, with all sites initially blocked
+    private final int BOTTOM;
+    private int TOP;
+    private final int n;
+    private boolean[][] openCells;
+    private final WeightedQuickUnionUF uf;
+    private int openCellCounter = 0;
+
+    // creates n-by-n openCells, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
+
+        this.n = n;
+        openCells = new boolean[n][n];
+        TOP = (n * n);
+        BOTTOM = (n * n) + 1;
+        uf = new WeightedQuickUnionUF((n * n) + 2);
+        unionTopToTopRow();
+        unionBottomToBottomRow();
     }
 
-//    // opens the site (row, col) if it is not open already
-//    public void open(int row, int col) {}
-//
-//    // is the site (row, col) open?
-//    public boolean isOpen(int row, int col) {}
-//
-//    // is the site (row, col) full?
-//    public boolean isFull(int row, int col) {}
-//
-//    // returns the number of open sites
-//    public int numberOfOpenSites() {}
-//
-//    // does the system percolate?
-//    public boolean percolates() {}
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validateInput(row, col);
+        setOpen(row, col);
+        unionWithNeighbours(row, col);
+    }
 
-//    // test client (optional)
-//    public static void main(String[] args) {}
+    // is the site (row, col) open?
+
+    public boolean isOpen(int row, int col) {
+        validateInput(row, col);
+        return getOpen(row, col);
+    }
+    // is the site (row, col) full?
+
+    public boolean isFull(int row, int col) {
+        validateInput(row, col);
+        return getOpen(row, col) && isConnectedToTopRow(row, col);
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return openCellCounter;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.find(TOP) == uf.find(BOTTOM);
+    }
+
+    private void unionWithNeighbours(int row, int col) {
+        unionIfPossible(row, col, row + 1, col);
+        unionIfPossible(row, col, row - 1, col);
+        unionIfPossible(row, col, row, col + 1);
+        unionIfPossible(row, col, row, col - 1);
+    }
+
+    private void unionIfPossible(int row1, int col1, int row2, int col2) {
+        if (withinGrid(row2, col2) && isOpen(row2, col2)) {
+            uf.union(rowColumnToIndex(row1, col1), rowColumnToIndex(row2, col2));
+        }
+    }
+
+    private boolean withinGrid(int row, int col) {
+        return row > 0 && row <= n && col > 0 && col <= n;
+    }
+
+    private boolean isConnectedToTopRow(int row, int col) {
+        int rootIndex = uf.find(rowColumnToIndex(row, col));
+        return rootIndex == TOP;
+    }
+
+    private void setOpen(int row, int col) {
+        if (!openCells[row - 1][col - 1]) openCellCounter++;
+        openCells[row - 1][col - 1] = true;
+    }
+
+    private boolean getOpen(int row, int col) {
+        return openCells[row - 1][col - 1];
+    }
+
+    private void unionTopToTopRow() {
+        for (int i = 1; i <= n; i++) {
+            uf.union(TOP, rowColumnToIndex(1, i));
+        }
+    }
+
+    private void unionBottomToBottomRow() {
+        for (int i = 1; i <= n; i++) {
+            uf.union(BOTTOM, rowColumnToIndex(n, i));
+        }
+    }
+
+    private int rowColumnToIndex(int row, int col) {
+        return (row - 1) * n + (col - 1);
+    }
+
+    private void validateInput(int row, int col) {
+        if (row < 1) throw new IllegalArgumentException();
+        if (col < 1) throw new IllegalArgumentException();
+        if (row > n) throw new IllegalArgumentException();
+        if (col > n) throw new IllegalArgumentException();
+    }
 }
--- test/PercolationTest.java
+++ test/PercolationTest.java
@@ -1,12 +1,112 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 class PercolationTest {
 
+    private Percolation percolation;
+
+    @BeforeEach
+    void setUp() {
+        this.percolation = new Percolation(5);
+    }
 
     @Test
     void shouldThrowErrorIfNLessThanOne() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Percolation(0));
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Percolation(-1));
+    }
+
+    @Test
+    void shouldOpenASquare() {
+        assertFalse(percolation.isOpen(1,1));
+        percolation.open(1,1);
+        assertTrue(percolation.isOpen(1,1));
+    }
+
+    @Test
+    void shouldOnlyBeFullIfOpen() {
+        assertFalse(percolation.isFull(1,1));
+        percolation.open(1,1);
+        assertTrue(percolation.isFull(1,1));
+    }
+
+    @Test
+    void topRowShouldBeFullWhenOpen() {
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+        percolation.open(1, 3);
+        percolation.open(1, 4);
+        assertTrue(percolation.isFull(1,1));
+        assertTrue(percolation.isFull(1,2));
+        assertTrue(percolation.isFull(1,3));
+        assertTrue(percolation.isFull(1,4));
+
+        assertFalse(percolation.isFull(1,5));
+    }
+
+    @Test
+    void shouldBeFullIfConnectedToTopOfGrid() {
+        percolation.open(1,1);
+        percolation.open(3,1);
+        assertFalse(percolation.isFull(3,1));
+
+        percolation.open(2,1);
+        assertTrue(percolation.isFull(3, 1));
+    }
+
+    @Test
+    void shouldPercolateIfTopConnectedToBottom() {
+        percolation.open(1,1);
+        percolation.open(2,1);
+        percolation.open(3,1);
+        percolation.open(4,1);
+        assertFalse(percolation.percolates());
+
+        percolation.open(4,2);
+        percolation.open(4,3);
+        percolation.open(4,4);
+        assertFalse(percolation.percolates());
+
+        percolation.open(5,4);
+        assertTrue(percolation.percolates());
+    }
+
+    @Test
+    void shouldCountTheNumberOfOpenCells() {
+        assertThat(percolation.numberOfOpenSites(), is(0));
+        percolation.open(1,1);
+        percolation.open(1,2);
+        assertThat(percolation.numberOfOpenSites(), is(2));
+        percolation.open(1,1);
+        assertThat(percolation.numberOfOpenSites(), is(2));
+    }
+
+    @Test
+    void methodsShouldThrowErrorIfOutsideRange() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.open(6, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.open(0, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.open(1, 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.open(1, 6));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isFull(6, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isFull(0, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isFull(1, 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isFull(1, 6));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isOpen(6, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isOpen(0, 1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isOpen(1, 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> percolation.isOpen(1, 6));
+
+        percolation.open(1, 5);
+        percolation.open(5, 1);
+        percolation.isOpen(1, 5);
+        percolation.isOpen(5, 1);
+        percolation.isFull(1, 5);
+        percolation.isFull(5, 1);
     }
 }
